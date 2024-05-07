0a0694a72149fcc25a4d5bf2ca330b9d2dcb7b14
Ville Korhonen <112542569+korhvi@users.noreply.github.com>
2024-05-07 19:37:39 +0300

    Update PostPage.tsx

--- src/pages/PostPage.tsx
+++ src/pages/PostPage.tsx
@@ -1,23 +1,70 @@
-import {
-    IonContent,
-    IonHeader,
-    IonPage,
-    IonTitle,
-    IonToolbar,
-} from '@ionic/react'
-import React from 'react'
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import React, { useEffect, useState } from 'react';
+import { db } from '../firebaseConfig';
+import { useParams } from 'react-router';
+
+interface PostData {
+    username: string;
+    title: string;
+    content: string;
+    date: string;
+    likes: number;
+    comments: number;
+}
 
 const PostPage: React.FC = () => {
+    const { postId } = useParams<{ postId: string }>();
+    const [postData, setPostData] = useState<PostData | null>(null);
+
+    useEffect(() => {
+        const fetchPostData = async () => {
+            try {
+                const postRef = db.collection('posts').doc(postId);
+                const doc = await postRef.get();
+                if (doc.exists) {
+                    const postData = doc.data() as PostData;
+                    setPostData(postData);
+                } else {
+                    console.log('Document not found');
+                }
+            } catch (error) {
+                console.error('Error fetching document:', error);
+            }
+        };
+        fetchPostData();
+    }, [postId]);
+
+    if (!postData) {
+        return (
+            <IonPage>
+                <IonHeader>
+                    <IonToolbar>
+                        <IonTitle>Post</IonTitle>
+                    </IonToolbar>
+                </IonHeader>
+                <IonContent>
+                    <p>Loading...</p>
+                </IonContent>
+            </IonPage>
+        );
+    }
+
     return (
         <IonPage>
             <IonHeader>
                 <IonToolbar>
-                    <IonTitle>Page Title</IonTitle>
+                    <IonTitle>{postData.title}</IonTitle>
                 </IonToolbar>
             </IonHeader>
-            <IonContent className="ion-padding">UI goes here...</IonContent>
+            <IonContent className="ion-padding">
+                <p>User: {postData.username}</p>
+                <p>Created: {postData.date}</p>
+                <p>Content: {postData.content}</p>
+                <p>Likes: {postData.likes}</p>
+                <p>Comments: {postData.comments}</p>
+            </IonContent>
         </IonPage>
-    )
+    );
 }
 
-export default PostPage
+export default PostPage;
