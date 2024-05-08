d68dd725ac4a6c005fe2bb61d9682b7e6081fbea
Ville Korhonen <112542569+korhvi@users.noreply.github.com>
2024-05-08 14:32:13 +0300

    Update PostPage.tsx

--- src/pages/PostPage.tsx
+++ src/pages/PostPage.tsx
@@ -1,38 +1,41 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React, { useEffect, useState } from 'react';
-import { db } from '../firebaseConfig';
-import { useParams } from 'react-router';
-
-interface PostData {
-    username: string;
-    title: string;
-    content: string;
-    date: string;
-    likes: number;
-    comments: number;
-}
+import {
+    IonButton,
+    IonButtons,
+    IonCard,
+    IonContent,
+    IonHeader,
+    IonIcon,
+    IonPage,
+    IonTitle,
+    IonToolbar,
+} from '@ionic/react'
+import React, { useEffect, useState } from 'react'
+import { db } from '../firebaseConfig'
+import { useParams } from 'react-router'
+import { PostData } from './ForumPage'
+import { arrowBack, chatboxEllipsesOutline, thumbsUpOutline } from 'ionicons/icons'
 
 const PostPage: React.FC = () => {
-    const { postId } = useParams<{ postId: string }>();
-    const [postData, setPostData] = useState<PostData | null>(null);
+    const { postId } = useParams<{ postId: string }>()
+    const [postData, setPostData] = useState<PostData | null>(null)
 
     useEffect(() => {
         const fetchPostData = async () => {
             try {
-                const postRef = db.collection('posts').doc(postId);
-                const doc = await postRef.get();
+                const postRef = db.collection('posts').doc(postId)
+                const doc = await postRef.get()
                 if (doc.exists) {
-                    const postData = doc.data() as PostData;
-                    setPostData(postData);
+                    const postData = doc.data() as PostData
+                    setPostData(postData)
                 } else {
-                    console.log('Document not found');
+                    console.log('Document not found')
                 }
             } catch (error) {
-                console.error('Error fetching document:', error);
+                console.error('Error fetching document:', error)
             }
-        };
-        fetchPostData();
-    }, [postId]);
+        }
+        fetchPostData()
+    }, [postId])
 
     if (!postData) {
         return (
@@ -46,25 +49,59 @@ const PostPage: React.FC = () => {
                     <p>Loading...</p>
                 </IonContent>
             </IonPage>
-        );
+        )
     }
 
     return (
         <IonPage>
             <IonHeader>
                 <IonToolbar>
+                    <IonButtons>
+                        <IonButton routerLink='/forumpage'>
+                            <IonIcon icon={arrowBack}></IonIcon>
+                        </IonButton>
+                    </IonButtons>
                     <IonTitle>{postData.title}</IonTitle>
                 </IonToolbar>
             </IonHeader>
             <IonContent className="ion-padding">
-                <p>User: {postData.username}</p>
-                <p>Created: {postData.date}</p>
-                <p>Content: {postData.content}</p>
-                <p>Likes: {postData.likes}</p>
-                <p>Comments: {postData.comments}</p>
+                <IonCard className="userPost">
+                    <div className="details">
+                        <p className="username">User: {postData.username}</p>
+                        <div className="date">
+                            <p>{postData.createdAt}</p>
+                        </div>
+                    </div>
+                    <div className="content">
+                        <h4 className="postTitle">{postData.title}</h4>
+                        <p className="textcontent">{postData.content}</p>
+                    </div>
+                    <div className="likesAndComments">
+                        <IonButton size='small' fill='clear' className="likes reactionCircle">
+                            <div>
+                            <IonIcon
+                                className="icon"
+                                icon={thumbsUpOutline}
+                            ></IonIcon>
+                            <span>{postData.likes}</span>
+                            </div>
+                        </IonButton>
+
+                        <IonButton size='small' fill='clear' className="comments reactionCircle">
+                            <div>
+                            <IonIcon
+                                className="icon"
+                                icon={chatboxEllipsesOutline}
+                            ></IonIcon>
+                            <span>{postData.comments}</span>
+                            </div>
+                        </IonButton>
+                    </div>
+                    
+                </IonCard>
             </IonContent>
         </IonPage>
-    );
+    )
 }
 
-export default PostPage;
+export default PostPage
